20a8585a0fde5fdd5c5acefdf4a605361c6f5601
umpu <carol.ledgers0h@icloud.com>
2024-03-27 20:03:37 +0500

    Initial Commit

---
+++ OreillyTodo/OreillyTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OreillyTodoApp.swift
+//  OreillyTodo
+//
+//  Created by Andrew Gorbunov on 27.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct OreillyTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
